a8b10e969a6ac517d4d550fcb3e5d2cbc3421a22
jiangtao <jtaobox@126.com>
2018-07-17 17:53:25 +0800

    添加meta info

--- src/component/V2exPost.js
+++ src/component/V2exPost.js
@@ -8,12 +8,27 @@ class V2exPost extends Component {
     this.props.getPostInfo(this.props.match.params.id)
   }
 
+  formatDate(timestamp) {
+    let time = (Date.now() / 1000 - timestamp).toFixed(0)
+    if (time <= 60) return time + '秒'
+    time = (time / 60).toFixed(0)
+    if (time <= 60) return time + '分钟'
+    time = (time / 60).toFixed(0)
+    if (time <= 24) return time + '小时'
+    time = (time / 24).toFixed(2)
+    return time + '天'
+
+  }
+
   render() {
     const { isFetching, info } = this.props
     return (
       <div className={ style.v2exPost }>
         { isFetching && <p>加载中...</p> }
         <h2 className={ style.title }>{ info.title }</h2>
+        <div className={ style.metaInfo }>
+          <span>By { info.member && info.member.username } at { this.formatDate(info.created) }前</span>
+        </div>
         <div className={ style.content } dangerouslySetInnerHTML ={{ __html: info.content_rendered }}></div>
       </div>
     )
